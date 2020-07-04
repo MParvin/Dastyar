4351b49d6f9572fd501f4f5f6e11c39329929dd1
Mohammad Parvin <m.parvin986@gmail.com>
2020-07-04 18:54:19 +0430

    install docker-compose added

---
+++ Install_docker-compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$UID" -ne 0 ]
+then
+echo "Please run this script with sudo"
+exit 1
+fi
+
+which docker-compose && (clear && echo "docker-compose already installed" && exit 1)
+
+downloadLink=`curl -sS https://api.github.com/repos/docker/compose/releases/latest |     grep -i \`uname -s\` | grep \`uname -i\` | grep download_url | cut -d'"' -f4 | had -1`
+
+wget $downloadLink -O /usr/local/bin/ || (clear && echo "Cannot download docker-compose && exit 1")
+
+chmod +x /usr/local/bin/docker-compose
+
+which docker-compose || (clear && echo "docker-compose installation failed && exit 1")
