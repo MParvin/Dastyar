39e3313de802453e53111f05f834b73993ec7a54
Mohammad Parvin <m.parvin986@gmail.com>
2020-09-21 10:54:07 +0330

    remove clear and correct double coats

--- Install_docker-compose.sh
+++ Install_docker-compose.sh
@@ -6,12 +6,12 @@ echo "Please run this script with sudo"
 exit 1
 fi
 
-which docker-compose && (clear && echo "docker-compose already installed" && exit 1)
+which docker-compose && (echo "docker-compose already installed" && exit 1)
 
-downloadLink=`curl -sS https://api.github.com/repos/docker/compose/releases/latest |     grep -i \`uname -s\` | grep \`uname -i\` | grep download_url | cut -d'"' -f4 | had -1`
+downloadLink=`curl -sS https://api.github.com/repos/docker/compose/releases/latest | grep -i \`uname -s\` | grep \`uname -i\` | grep download_url | cut -d'"' -f4 | head -1`
 
-wget $downloadLink -O /usr/local/bin/ || (clear && echo "Cannot download docker-compose && exit 1")
+wget $downloadLink -O /usr/local/bin/ || (echo "Cannot download docker-compose" && exit 1)
 
 chmod +x /usr/local/bin/docker-compose
 
-which docker-compose || (clear && echo "docker-compose installation failed && exit 1")
+which docker-compose || (clear && echo "docker-compose installation failed" && exit 1)
