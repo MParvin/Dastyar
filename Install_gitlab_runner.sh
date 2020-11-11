48b07ba3f23a3a5ebd7b7ce1d72626b12e0528da
Mohammad Parvin <m.parvin986@gmail.com>
2020-11-11 11:05:54 +0330

    add gitlab-runner

---
+++ Install_gitlab_runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ToDo: add other archtiectures (arm and x86) 
+
+wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+chmod +x /usr/local/bin/gitlab-runner
+
+useradd --comment "Gitlab runner" --create-home gitlab-runner --shell /bin/bash
+
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+
+gitlab-runner start
